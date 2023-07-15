3e874fb0f3bb681d5b6c53d3a4acdea80e100bd0
Alfred-Onuada <aonuada5@gmail.com>
2023-07-15 23:44:22 +0100

    built models for users

--- models/donor.model.js
+++ models/donor.model.js
@@ -1,7 +1,43 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const donorSchema = new mongoose.Schema({
-
+  fullName: {
+    type: String,
+    required: [true, "Please specify a Full Name"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please specify an Email"],
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: [true, "Please specify a Phone Number"],
+    unique: true
+  },
+  age: {
+    type: Number,
+    required: [true, "Please specify an Age"]
+  },
+  bloodGroup: {
+    type: String,
+    required: [true, "Please specify a Blood Group"]
+  },
+  genotype: {
+    type: String,
+    required: [true, "Please specify a Genotype"]
+  },
+  password: {
+    type: String,
+    required: [true, "Please specify a Password"]
+  }
 }, { timestamps: true })
+
+donorSchema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  
+  next()
+})
 
 const donorModel = mongoose.Model('donor', donorSchema);
--- models/hospital.model.js
+++ models/hospital.model.js
@@ -1,7 +1,34 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const hospitalSchema = new mongoose.Schema({
-
+  hospitalName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true })
+
+hospitalSchema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  
+  next();
+});
 
 const hospitalModel = mongoose.Model('hospital', hospitalSchema);
